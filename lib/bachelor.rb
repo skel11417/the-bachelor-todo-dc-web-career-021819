14dd04b816da50767716f05e2792c856fa251a8f
Sean Kelly <shonkelli@gmail.com>
2019-02-06 17:39:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -28,7 +28,12 @@ end
 def get_contestant_name(data, occupation)
   # code here
   data.each do |seasons, contestants|
-    
+    contestants.each do |contestant|
+      if contestant["occupation"] = occupation
+        return contestant["name"]
+      end 
+    end 
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
